09788091e5868f3299e6f8695677ed1f6de6ccf1
Luis Menina <liberforce@freeside.fr>
2020-01-19 17:53:24 +0100

    Change default pelican social media links

--- pelicanconf.py
+++ pelicanconf.py
@@ -22,16 +22,13 @@ AUTHOR_FEED_RSS = None
 
 # Blogroll
 LINKS = (
-    ("Pelican", "http://getpelican.com/"),
-    ("Python.org", "http://python.org/"),
-    ("Jinja2", "http://jinja.pocoo.org/"),
-    ("You can modify those links in your config file", "#"),
+    ("GNOME", "http://www.gnome.org"),
+    ("Mageia", "http://www.mageia.org"),
 )
 
 # Social widget
 SOCIAL = (
-    ("You can add links in your config file", "#"),
-    ("Another social link", "#"),
+    ("Mastodon", "https://framapiaf.org/@liberforce"),
 )
 
 DEFAULT_PAGINATION = 10
