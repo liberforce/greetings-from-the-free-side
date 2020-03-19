81fabcdef7c2ae4ae2371a58c54f2b070cdddc36
Luis Menina <liberforce@freeside.fr>
2020-03-19 23:31:07 +0100

    Add first test parsing a single dotclear entry

---
+++ tests/parser/test_dotclear.py
@@ -0,0 +1,20 @@
+import gandiblog2pelican.importer as imp
+
+def test_simple():
+    field_gen = imp.dc2fields("tests/data/simple.txt")
+    fields = []
+    for fieldset in field_gen:
+        for field in fieldset:
+            fields.append(field)
+
+    assert fields[0] == "En direct d'Istanbul" # title
+    assert fields[1] == "post_content_xhtml" # content
+    assert fields[2] == "en-direct-distanbul" # filename
+    #assert fields[3] == "2008-07-07 11:07" # date
+    assert fields[4] == "" # author
+    assert fields[5] == ["Life"] # categories
+    #assert fields[6] == ["GNOME", "GUADEC"] # tags
+    assert fields[7] == "published" # status
+    assert fields[8] == "article" # kind
+    assert fields[9] == "html" # format
+
