a5f6f83c101615874a262e25f252a8b8899ec6ca
Luis Menina <liberforce@freeside.fr>
2020-03-20 02:07:03 +0100

    Reformat

--- tests/parsers/test_dotclear_parser.py
+++ tests/parsers/test_dotclear_parser.py
@@ -1,5 +1,6 @@
 import gandiblog2pelican.importer as imp
 
+
 def test_simple():
     field_gen = imp.dc2fields("tests/data/simple.txt")
     fields = []
@@ -7,14 +8,13 @@ def test_simple():
         for field in fieldset:
             fields.append(field)
 
-    assert fields[0] == "En direct d'Istanbul" # title
-    assert fields[1] == "post_content_xhtml" # content
-    assert fields[2] == "en-direct-distanbul" # filename
-    assert fields[3] == "2008-07-07 11:07" # date
-    assert fields[4] == "TEST-GANDI" # author
-    assert fields[5] == ["Life"] # categories
-    assert fields[6] == ["GUADEC", "GNOME"] # tags
-    assert fields[7] == "published" # status
-    assert fields[8] == "article" # kind
-    assert fields[9] == "html" # format
-
+    assert fields[0] == "En direct d'Istanbul"  # title
+    assert fields[1] == "post_content_xhtml"  # content
+    assert fields[2] == "en-direct-distanbul"  # filename
+    assert fields[3] == "2008-07-07 11:07"  # date
+    assert fields[4] == "TEST-GANDI"  # author
+    assert fields[5] == ["Life"]  # categories
+    assert fields[6] == ["GUADEC", "GNOME"]  # tags
+    assert fields[7] == "published"  # status
+    assert fields[8] == "article"  # kind
+    assert fields[9] == "html"  # format
