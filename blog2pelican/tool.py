ba6551a8033d789634bf70fbae189a50db91f5df
Luis Menina <liberforce@freeside.fr>
2020-03-21 02:36:40 +0100

    Use subcommands to show which options are backend-specific

--- blog2pelican/tool.py
+++ blog2pelican/tool.py
@@ -17,17 +17,27 @@ from blog2pelican.renderers.common import (
 
 def create_argument_parser():
     parser = argparse.ArgumentParser(
-        description="Transform feed, Blogger, Dotclear, Posterous, Tumblr, or"
+        description="Transform feed, Blogger, Dotclear, Posterous, Tumblr, or "
         "WordPress files into reST (rst) or Markdown (md) files. "
         "Be sure to have pandoc installed.",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
 
-    parser.add_argument(
-        "engine",
-        choices=["blogger", "dotclear", "posterous", "tumblr", "wordpress", "feed"],
-        help="Format of the input file",
-    )
+    subparsers = parser.add_subparsers(dest="engine")
+    cmdparsers = {}
+
+    for engine in [
+        "blogger",
+        "dotclear",
+        "posterous",
+        "tumblr",
+        "wordpress",
+        "feed",
+    ]:
+        cmdparsers[engine] = subparsers.add_parser(
+            engine, help="Import from %s" % engine
+        )
+
     parser.add_argument(dest="input", help="The input file to read")
     parser.add_argument(
         "-o", "--output", dest="output", default="content", help="Output path"
@@ -46,47 +56,50 @@ def create_argument_parser():
         dest="dircat",
         help="Put files in directories with categories name",
     )
-    parser.add_argument(
-        "--dir-page",
-        action="store_true",
-        dest="dirpage",
-        help=(
-            'Put files recognised as pages in "pages/" sub-directory'
-            " (blogger and wordpress import only)"
-        ),
-    )
+
+    for engine in ["blogger", "wordpress"]:
+        cmdparser = cmdparsers[engine]
+        cmdparser.add_argument(
+            "--dir-page",
+            action="store_true",
+            dest="dirpage",
+            help=('Put files recognised as pages in "pages/" sub-directory'),
+        )
     parser.add_argument(
         "--filter-author",
         dest="author",
         help="Import only post from the specified author",
     )
-    parser.add_argument(
-        "--strip-raw",
-        action="store_true",
-        dest="strip_raw",
-        help="Strip raw HTML code that can't be converted to "
-        "markup such as flash embeds or iframes (wordpress import only)",
-    )
-    parser.add_argument(
-        "--wp-custpost",
-        action="store_true",
-        dest="wp_custpost",
-        help="Put wordpress custom post types in directories. If used with "
-        "--dir-cat option directories will be created as "
-        "/post_type/category/ (wordpress import only)",
-    )
-    parser.add_argument(
-        "--wp-attach",
-        action="store_true",
-        dest="wp_attach",
-        help="(wordpress import only) Download files uploaded to wordpress as "
-        "attachments. Files will be added to posts as a list in the post "
-        "header. All files will be downloaded, even if "
-        "they aren't associated with a post. Files will be downloaded "
-        "with their original path inside the output directory. "
-        "e.g. output/wp-uploads/date/postname/file.jpg "
-        "-- Requires an internet connection --",
-    )
+
+    for engine in ["wordpress"]:
+        cmdparser = cmdparsers[engine]
+        cmdparser.add_argument(
+            "--strip-raw",
+            action="store_true",
+            dest="strip_raw",
+            help="Strip raw HTML code that can't be converted to "
+            "markup such as flash embeds or iframes",
+        )
+        cmdparser.add_argument(
+            "--wp-custpost",
+            action="store_true",
+            dest="wp_custpost",
+            help="Put wordpress custom post types in directories. If used with "
+            "--dir-cat option directories will be created as "
+            "/post_type/category/",
+        )
+        cmdparser.add_argument(
+            "--wp-attach",
+            action="store_true",
+            dest="wp_attach",
+            help=" Download files uploaded to wordpress as "
+            "attachments. Files will be added to posts as a list in the post "
+            "header. All files will be downloaded, even if "
+            "they aren't associated with a post. Files will be downloaded "
+            "with their original path inside the output directory. "
+            "e.g. output/wp-uploads/date/postname/file.jpg "
+            "-- Requires an internet connection --",
+        )
     parser.add_argument(
         "--disable-slugs",
         action="store_true",
@@ -95,24 +108,20 @@ def create_argument_parser():
         "With this disabled, your Pelican URLs may not be consistent "
         "with your original posts.",
     )
-    parser.add_argument(
-        "-e",
-        "--email",
-        dest="email",
-        help="Email address (posterous import only)",
-    )
-    parser.add_argument(
-        "-p",
-        "--password",
-        dest="password",
-        help="Password (posterous import only)",
-    )
-    parser.add_argument(
-        "-b",
-        "--blogname",
-        dest="blogname",
-        help="Blog name (Tumblr import only)",
-    )
+
+    for engine in ["posterous"]:
+        cmdparser = cmdparsers[engine]
+        cmdparser.add_argument(
+            "-e", "--email", dest="email", help="Email address",
+        )
+        cmdparser.add_argument(
+            "-p", "--password", dest="password", help="Password",
+        )
+    for engine in ["tumblr"]:
+        cmdparser = cmdparsers[engine]
+        cmdparser.add_argument(
+            "-b", "--blogname", dest="blogname", help="Blog name",
+        )
     return parser
 
 
@@ -127,17 +136,10 @@ def main():
             error = "Unable to create the output folder: " + args.output
             exit(error)
 
-    if args.wp_attach and args.engine != "wordpress":
-        error = (
-            "You must be importing a wordpress xml "
-            "to use the --wp-attach option"
-        )
-        exit(error)
-
     parser = blog2pelican.parsers.make_parser(args.engine, args)
     fields = parser.parse()
 
-    if args.wp_attach:
+    if "wp_attach" in args and args.wp_attach:
         attachments = get_attachments(args.input)
     else:
         attachments = None
@@ -149,12 +151,12 @@ def main():
         args.markup,
         args.output,
         dircat=args.dircat or False,
-        dirpage=args.dirpage or False,
-        strip_raw=args.strip_raw or False,
-        disable_slugs=args.disable_slugs or False,
+        dirpage="dirpage" in args and args.dirpage or False,
+        strip_raw="strip_raw" in args and args.strip_raw or False,
+        disable_slugs="disable_slugs" in args and args.disable_slugs or False,
         filter_author=args.author,
-        wp_custpost=args.wp_custpost or False,
-        wp_attach=args.wp_attach or False,
+        wp_custpost="wp_custpost" in args and args.wp_custpost or False,
+        wp_attach="wp_attach" in args and args.wp_attach or False,
         attachments=attachments or None,
     )
 
